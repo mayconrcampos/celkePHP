a384e784f6807d1ea048a89ecb41df1cd82b0de5
mayconrcampos <maycon.campos@gmail.com>
2022-01-24 12:51:58 -0300

    Projeto CRUD com ReactJS e PHP

---
+++ ReactJS/meu-app2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Cabecalho from "./Cabecalho";
+
+
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Cabecalho />
+        
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ ReactJS/meu-app2/src/Cabecalho.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Cabecalho(){
+    return(
+        <h1>Porcaria de cabeçalho</h1>
+    );
+}
+
+export default Cabecalho;
